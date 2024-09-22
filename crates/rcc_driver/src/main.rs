63fb602c75d8c2d146921ef9c65e8e1f99c8b0a3
Christian Crespo <christian.crespoo.98@gmail.com>
2024-09-22 10:23:35 +0200

    fix(driver): only emit assembly when `--compile` flag is passed, and emit binary when no flag is given

--- crates/rcc_driver/src/main.rs
+++ crates/rcc_driver/src/main.rs
@@ -38,12 +38,14 @@ fn main() {
         parse(&args.filename);
     } else if args.codegen {
         codegen(&args.filename)
+    } else if args.compile {
+        compile(&args.filename)
     } else {
-        let preprocessor_output =
-            spawn_preprocessor(&args.filename).expect("Failed to spawn preprocessor.");
-        let compiler_output =
-            spawn_compiler(preprocessor_output).expect("Failed to spawn compiler.");
-        assemble(compiler_output).expect("Failed to spawn assembler");
+        compile(&args.filename);
+        assemble(
+            &args.filename.replace(".c", ".s"),
+            args.filename.trim_end_matches(".c"),
+        ).expect("Failed to assemble program.");
     }
 }
 
@@ -93,6 +95,23 @@ fn codegen(filename: &str) {
     let codegen = Codegen::new();
     let asm = codegen.codegen(&program);
     println!("{:#?}", asm);
+}
+
+fn compile(filename: &str) {
+    let source = std::fs::read_to_string(filename).expect("Failed to read file.");
+
+    let mut interner = Interner::new();
+    let parser = Parser::new(&source, &mut interner);
+    let program = match parser.parse() {
+        Ok(program) => program,
+        Err(err) => {
+            println!("{:?}", err.with_source_code(source.clone()));
+            std::process::exit(1)
+        }
+    };
+
+    let codegen = Codegen::new();
+    let asm = codegen.codegen(&program);
 
     let file = File::create(filename.replace(".c", ".s")).expect("Failed to create output file");
     let codeemit = CodeEmit::new(&file, &interner);
@@ -106,29 +125,8 @@ fn spawn_preprocessor(filename: &str) -> std::io::Result<Child> {
         .spawn()
 }
 
-fn spawn_compiler(preprocessor_output: Child) -> std::io::Result<Child> {
-    Command::new("gcc")
-        .args([
-            "-S",
-            "-O",
-            "-fno-asynchronous-unwind-tables",
-            "-fcf-protection=none",
-            "-x",
-            "c",
-            "-",
-            "-o",
-            "-",
-        ])
-        .stdin(preprocessor_output.stdout.unwrap())
-        .stdout(Stdio::piped())
-        .spawn()
-}
-
-fn assemble(compiler_output: Child) -> std::io::Result<()> {
-    Command::new("gcc")
-        .args(["-x", "assembler", "-", "-o", "output"])
-        .stdin(compiler_output.stdout.unwrap())
-        .status()?;
+fn assemble(input: &str, output: &str) -> std::io::Result<()> {
+    Command::new("gcc").args([input, "-o", output]).status()?;
 
     Ok(())
 }
