d1a2f2c01fcb375fb59c1ad4e2987c2d415453e9
Christian Crespo <christian.crespoo.98@gmail.com>
2024-11-08 22:22:48 +0100

    fix(semantics): collect all labels in a function before attempting to resolve gotos

--- crates/rcc_semantics/src/labels.rs
+++ crates/rcc_semantics/src/labels.rs
@@ -1,33 +1,46 @@
 use std::collections::{hash_map::Entry, HashMap};
 
 use rcc_ast::{
-    Block, BlockItem, FunctionDeclaration, GotoStatement, IfStatement, Label, LabeledStatement,
-    Program, Statement,
+    visit_mut::{self, VisitMut},
+    FunctionDeclaration, GotoStatement, Label, Program,
 };
 use rcc_interner::Symbol;
 
 use crate::{diagnostics, ResolutionContext};
 
-#[derive(Debug, Default)]
-struct Scope {
-    labels: HashMap<Symbol, Label>,
+pub fn resolve<'a, 'src>(res: &'a mut ResolutionContext<'_, 'src>, program: &'a Program<'src>) {
+    resolve_program(res, program);
 }
 
-struct LabelResolutionContext<'a, 'res, 'src> {
+fn resolve_program<'a, 'src>(res: &'a mut ResolutionContext<'_, 'src>, program: &'a Program<'src>) {
+    resolve_func_decl(res, &program.func)
+}
+
+fn resolve_func_decl<'a, 'src>(
+    res: &'a mut ResolutionContext<'_, 'src>,
+    decl: &'a FunctionDeclaration<'src>,
+) {
+    let labels = LabelCollector::new(res).collect_labels(decl);
+    LabelResolver::new(res, &labels).resolve_labels(decl);
+}
+
+type Labels = HashMap<Symbol, Label>;
+
+struct LabelCollector<'a, 'res, 'src> {
     res: &'res mut ResolutionContext<'a, 'src>,
-    scope: Scope,
+    labels: Labels,
 }
 
-impl<'a, 'res, 'src> LabelResolutionContext<'a, 'res, 'src> {
-    fn new(res: &'res mut ResolutionContext<'a, 'src>) -> LabelResolutionContext<'a, 'res, 'src> {
-        LabelResolutionContext {
+impl<'a, 'res, 'src> LabelCollector<'a, 'res, 'src> {
+    fn new(res: &'res mut ResolutionContext<'a, 'src>) -> LabelCollector<'a, 'res, 'src> {
+        LabelCollector {
             res,
-            scope: Scope::default(),
+            labels: HashMap::new(),
         }
     }
 
     fn define_label(&mut self, label: Label) {
-        match self.scope.labels.entry(label.symbol) {
+        match self.labels.entry(label.symbol) {
             Entry::Vacant(entry) => {
                 entry.insert(label);
             }
@@ -40,72 +53,46 @@ impl<'a, 'res, 'src> LabelResolutionContext<'a, 'res, 'src> {
         };
     }
 
+    fn collect_labels(mut self, decl: &FunctionDeclaration<'src>) -> Labels {
+        self.visit_func_decl(decl);
+        self.labels
+    }
+}
+
+impl<'a, 'res, 'src> VisitMut<'src> for LabelCollector<'a, 'res, 'src> {
+    fn visit_labeled_stmt(&mut self, stmt: &rcc_ast::LabeledStatement<'src>) {
+        self.define_label(stmt.label);
+        visit_mut::walk_stmt(self, &stmt.stmt);
+    }
+}
+
+struct LabelResolver<'a, 'res, 'src> {
+    res: &'a mut ResolutionContext<'res, 'src>,
+    labels: &'a HashMap<Symbol, Label>,
+}
+
+impl<'a, 'res, 'src> LabelResolver<'a, 'res, 'src> {
+    fn new(
+        res: &'a mut ResolutionContext<'res, 'src>,
+        labels: &'a Labels,
+    ) -> LabelResolver<'a, 'res, 'src> {
+        LabelResolver { res, labels }
+    }
+
     fn lookup_label(&mut self, label: Label) {
-        if !self.scope.labels.contains_key(&label.symbol) {
+        if !self.labels.contains_key(&label.symbol) {
             let id = self.res.interner.get(label.symbol);
             self.res.error(diagnostics::undefined_label(id, label.span))
         }
     }
 
-    fn clear_scope(&mut self) {
-        self.scope = Scope::default();
-    }
-
-    fn scoped<T>(&mut self, f: impl FnOnce(&mut LabelResolutionContext) -> T) -> T {
-        let result = f(self);
-        self.clear_scope();
-
-        result
+    fn resolve_labels(mut self, decl: &FunctionDeclaration<'src>) {
+        self.visit_func_decl(decl);
     }
 }
 
-pub fn resolve(res: &mut ResolutionContext, program: &Program) {
-    let mut cx = LabelResolutionContext::new(res);
-    resolve_program(&mut cx, program)
-}
-
-fn resolve_program(cx: &mut LabelResolutionContext, program: &Program) {
-    resolve_func_decl(cx, &program.func)
-}
-
-fn resolve_func_decl(cx: &mut LabelResolutionContext, decl: &FunctionDeclaration) {
-    cx.scoped(|cx| resolve_block(cx, &decl.body))
-}
-
-fn resolve_block(cx: &mut LabelResolutionContext, block: &Block) {
-    for item in &block.items {
-        resolve_block_item(cx, item)
+impl<'a, 'res, 'src> VisitMut<'src> for LabelResolver<'a, 'res, 'src> {
+    fn visit_goto_stmt(&mut self, stmt: &GotoStatement) {
+        self.lookup_label(stmt.label);
     }
-}
-
-fn resolve_block_item(cx: &mut LabelResolutionContext, block_item: &BlockItem) {
-    match block_item {
-        BlockItem::Declaration(_) => {}
-        BlockItem::Statement(stmt) => resolve_stmt(cx, stmt),
-    }
-}
-
-fn resolve_stmt(cx: &mut LabelResolutionContext, stmt: &Statement) {
-    match stmt {
-        Statement::Goto(stmt) => resolve_goto_stmt(cx, stmt),
-        Statement::If(stmt) => resolve_if_stmt(cx, stmt),
-        Statement::Labeled(stmt) => resolve_labeled_stmt(cx, stmt),
-        _ => {}
-    }
-}
-
-fn resolve_goto_stmt(cx: &mut LabelResolutionContext, stmt: &GotoStatement) {
-    cx.lookup_label(stmt.label);
-}
-
-fn resolve_if_stmt(cx: &mut LabelResolutionContext, stmt: &IfStatement) {
-    resolve_stmt(cx, &stmt.consequent);
-
-    if let Some(alternate) = &stmt.alternate {
-        resolve_stmt(cx, alternate);
-    }
-}
-
-fn resolve_labeled_stmt(cx: &mut LabelResolutionContext, stmt: &LabeledStatement) {
-    cx.define_label(stmt.label)
 }
